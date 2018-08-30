006dff37e4f6065a7739852cd0e2fe2795b4c9f7
Todd Baylis <todd.baylis@widerfunnel.com>
2018-08-30 15:31:31 -0700

    Add optimizely SDK files (in progress)

---
+++ __process/process.php
@@ -0,0 +1,125 @@
+<?php 
+
+// PHP SDK Demo App
+// Copyright 2017 Optimizely. Licensed under the Apache License
+// View the documentation: http://bit.ly/2rfsbxt
+
+  require_once 'vendor/autoload.php';
+  use Optimizely\Optimizely;
+  use Optimizely\Logger\DefaultLogger;
+
+  $PROJECT_ID = '11354124779';
+
+
+  if(isset($_POST['track'])){
+    track($_POST['track']);
+  }
+
+  function setUserId() {
+    $cookie_name = "user";
+    $cookie_value = "John Doe";
+    // check if cookie exists
+    if(isset($_COOKIE[$cookie_name])) {
+      return $_COOKIE[$cookie_name];
+    } else {
+      // if no cookie exists, set cookie
+      setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+    }
+  }
+
+
+  function initOpti(){
+    $project_id = $GLOBALS['PROJECT_ID'];
+    $url = "https://cdn.optimizely.com/json/$project_id.json";
+    $datafile = file_get_contents($url);
+    //$optimizelyClient = new Optimizely($datafile, null, new DefaultLogger(logger.DEBUG));
+    $optimizelyClient = new Optimizely($datafile);
+
+    $userId = setUserId();
+    
+    $variation = $optimizelyClient->activate('dsat-todd-php', $userId);
+    if ($variation == 'control') {
+      // execute code for control
+    } else if ($variation == 'varA') {
+      // execute code for varA
+    } else {
+     // execute default code
+    }
+
+    return $optimizelyClient;
+  }
+
+  function track($user_id){
+    $client = initOpti();
+    $client->track('BUY', $user_id);
+
+    header('Location: purchase.html');
+  }
+
+
+  function getItems($user_id = NULL){
+    $csvFile = file('items.csv');
+    $data = [];
+    foreach ($csvFile as $line) {
+      $data[] = str_getcsv($line);
+    }
+
+    if(isset($user_id)){
+      $client = initOpti();
+
+      $variation = $client->activate('ITEM_SORT', $user_id);
+    
+      if ($variation == 'PRICE') {
+        echo "<h3>PRICE</h3>";
+        foreach ($data as $key => $row) {
+          $num = str_replace('$', '', $row[3]);
+          $num = (int)$num;
+          $price[$key] = $num;
+        }
+      
+        array_multisort($price, SORT_ASC, $data);
+      
+      } elseif ($variation == 'CATEGORY') {  
+          echo "<h3>CATEGORY</h3>";
+          foreach ($data as $key => $row) {
+            $category[$key] = $row[2];
+           }
+        
+          array_multisort($category, SORT_ASC, $data);
+      } 
+      return array($data, $variation);
+
+    } else {		
+      return array($data, NULL);
+    }
+  }
+
+  function displayItems($items){
+    
+    for($i=0;$i<9;$i++){
+      $item_name = $items[$i][0];
+      $item_color = $items[$i][1];
+      $item_category = $items[$i][2];
+      $item_price = $items[$i][3];
+      $item_url = $items[$i][4];
+
+      echo "<td class='background--white text--center font-family--tahoma'>";
+      echo "<h2><b> $item_name</b></h2>";
+      echo " in $item_color <br>";
+      echo "<b>$item_category, $item_price </b>";
+      echo "<img src='images/$item_url'>";
+      echo "<form action='process.php' method='post'> 
+           <input type='text' name='track' value='$user_id' hidden='true'>";
+      echo "<button type='submit' style='background: #cb1b2c; color: #ffffff; border: none'> BUY NOW</button>";
+      echo "</form>";
+      echo "</td>";
+      echo "<td width='10'></td>";
+      echo "<p></p>";
+      // every 3 rows
+      if (($i+1) % 3 == 0) {
+      echo "</tr>";
+      }
+    }    
+  }
+
+?>
